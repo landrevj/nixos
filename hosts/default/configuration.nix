2b2e8c22f87a1da4ceea75ed0723fe78a1716d5f
Joseph Landreville <landrevillejoseph@gmail.com>
2024-03-11 02:05:47 -0700

    switch to stable beta nvidia driver

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -69,7 +69,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
   boot.extraModprobeConfig = ''
     options nvidia NVreg_PreserveVideoMemoryAllocations=1
