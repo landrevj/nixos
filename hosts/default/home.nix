12a1283ddf9a58ebf94c386313c9db4ab0aada69
Joseph Landreville <landrevillejoseph@gmail.com>
2024-03-12 17:48:18 -0700

    add gallery-dl

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -57,6 +57,9 @@
     gpu-screen-recorder
     gpu-screen-recorder-gtk
 
+    # downloaders
+    gallery-dl
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
