cb428fe917bd1ffed3283acfcac1410ae96e151a
Joseph Landreville <landrevillejoseph@gmail.com>
2024-03-20 11:19:41 -0700

    allow ntfs

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -28,7 +28,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  boot.supportedFilesystems = [ "ntfs" ];
 
   # Enable networking
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
