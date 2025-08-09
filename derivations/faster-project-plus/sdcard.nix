80a7954b7ee82a20ec898bdb2e9052f440a84a88
Joseph Landreville <landrevillejoseph@gmail.com>
2025-08-08 23:30:58 -0700

    forgot a file

--- derivations/faster-project-plus/sdcard.nix
+++ derivations/faster-project-plus/sdcard.nix
@@ -5,7 +5,7 @@
 
 stdenv.mkDerivation {
   name = "fpp-sdcard";
-  version = "3.0.2";
+  version = "3.1.2";
 
   src = fetchzip {
     url = "https://github.com/jlambert360/FPM-AppImage/releases/download/v3.1.2/sd.tar.gz";
