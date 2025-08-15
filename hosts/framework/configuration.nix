16e13ffa3007fd17f155509ee225d31c76272961
Joseph Landreville <landrevillejoseph@gmail.com>
2025-08-15 01:00:38 -0700

    quietboot

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -35,6 +35,34 @@
     pkiBundle = "/var/lib/sbctl";
   };
 
+  # quietboot
+  console = {
+    useXkbConfig = true;
+    earlySetup = true;
+  };
+  boot = {
+    initrd = {
+      systemd.enable = true; # start earlier so plymouth asks for luks password
+      verbose = false;
+    };
+    plymouth = {
+      enable = true;
+      theme = "bgrt";
+    };
+    loader.timeout = 0;
+    consoleLogLevel = 0;
+    kernelParams = [
+      "plymouth.use-simpledrm"
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+  };
+
   # Secrets
   sops = {
     defaultSopsFile = ../../secrets/framework/secrets.yaml;
