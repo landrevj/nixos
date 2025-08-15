eb8675a48794c5716bb75357a303670c259f631b
Joseph Landreville <landrevillejoseph@gmail.com>
2025-08-15 12:10:59 -0700

    customize firefox manually

--- modules/home/applications/firefox.nix
+++ modules/home/applications/firefox.nix
@@ -13,6 +13,25 @@
         ff2mpv-rust
         kdePackages.plasma-browser-integration
       ];
+      profiles."landrevj.default" = {
+        settings = {
+          "browser.toolbars.bookmarks.visibility" = "never";
+          "sidebar.revamp" = true;
+          "sidebar.verticalTabs" = true;
+          "sidebar.visibility" = "hide-sidebar";
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        };
+        userChrome = ''
+          #sidebar-main,
+          #sidebar-panel-header {
+            display: none;
+          }
+
+          #sidebar-box {
+            padding: 0 !important;
+          }
+        '';
+      };
     };
   };
 }
