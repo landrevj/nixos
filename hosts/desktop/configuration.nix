c7b79f6964cf01de778b88510b8e6b4ebf7b88da
Joseph Landreville <landrevillejoseph@gmail.com>
2025-11-24 07:10:47 +0000

    typo

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -7,7 +7,7 @@
 
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05" # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 
   # Set kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
